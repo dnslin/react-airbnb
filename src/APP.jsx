41f1b48e309098e9ac096c7e71fc595de702e064
dnslin <dnslint@gmail.com>
2023-04-02 16:14:27 +0800

    feat: 去除 creat-react-app的构建的无用代码

---
+++ src/APP.jsx
@@ -0,0 +1,22 @@
+/**
+ * @name: APP.jsx
+ * @projectName: React-Project
+ * @description: 入口文件
+ * @dirPath: react-airbnb/src
+ * @author: DnsLin
+ * @organizationName: dnslin.com
+ * @time: 2023/4/2
+ */
+import React, {PureComponent} from 'react';
+
+export class APP extends PureComponent {
+    render() {
+        return (
+            <div>
+                APP
+            </div>
+        );
+    }
+}
+
+export default APP;
